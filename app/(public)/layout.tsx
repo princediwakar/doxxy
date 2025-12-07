cd390464da189b1416e9b861ee769d797cf7aeb3
Prince Diwakar <princediwakar25@gmail.com>
2025-12-07 12:33:25 +0530

    added google tag manager

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,4 +1,5 @@
 import { AppHeader } from '@/components/AppHeader'
+import Script from 'next/script'
 
 export default function PublicLayout({
   children,
@@ -7,6 +8,33 @@ export default function PublicLayout({
 }) {
   return (
     <>
+      {/* Google Tag Manager */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PW8LFQ97');
+          `,
+        }}
+      />
+      {/* End Google Tag Manager */}
+
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-PW8LFQ97"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
+      {/* End Google Tag Manager (noscript) */}
+
       <AppHeader />
       {children}
     </>
